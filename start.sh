e7e7428c5310af63302b2d6548476490e9184f79
RDX RAJ <122299830+igraj147@users.noreply.github.com>
2023-11-10 09:32:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/LazyDeveloperr/LazyPrincessV2 /LazyPrincessV2
+  git clone https://github.com/igraj147/RDXRAJ_MUSIC_BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /LazyPrincessV2
+  git clone $UPSTREAM_REPO /RDXRAJ_MUSIC_BOT
 fi
-cd /LazyPrincessV2
+cd /RDXRAJ_MUSIC_BOT
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
